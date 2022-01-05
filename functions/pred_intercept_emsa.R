491c7417ff9238ef5a0a8ba019809da4823580ed
nlhopkins <natashahopkins1@hotmail.co.uk>
2022-01-05 20:20:56 +0000

    introduced if() function to introduce intercept

--- functions/pred_intercept_emsa.R
+++ functions/pred_intercept_emsa.R
@@ -1,3 +1,7 @@
 pred_intercept_emsa <- function(x) {
-    (emsa_rf * x) / (emsa_intercept  + x)
+    if (emsa$rf[1] > 0) {
+        (emsa_rf * x) / (emsa_intercept  + x) + (summary(emsa_fit)$coefficients[3, 1])
+    } else{
+        (emsa_rf * x) / (emsa_intercept  + x)
+    }
 }
