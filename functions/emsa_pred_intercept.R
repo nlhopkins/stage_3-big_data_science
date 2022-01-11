cc83e9f9476b5c930993686182748126065f09df
nlhopkins <natashahopkins1@hotmail.co.uk>
2022-01-11 17:37:19 +0000

    Corrected

--- functions/emsa_pred_intercept.R
+++ functions/emsa_pred_intercept.R
@@ -2,6 +2,6 @@ emsa_pred_intercept <- function(x) {
     if (emsa$rf[1] > 0) {
         ((summary(NLS(emsa))$coefficients[1, 1]) * x) / ((summary(NLS(emsa))$coefficients[2, 1])  + x) + (summary(NLS(emsa))$coefficients[3, 1])
     } else{
-        ((summary(NLS(emsa))$coefficients[1, 1]) * x) / ((summary(NLS(emsa))$coefficients[2, 1])  + x)
+        ((summary(NLS(emsa))$coefficients[1, 1]) * x) / ((summary(NLS(emsa))$coefficients[2, 1]))
     }
 }
